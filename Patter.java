8346dcf412a2e5fda16c7892e771afb894dd85df
skjha98 <skjha98@gmail.com>
2018-05-04 12:59:58 +0530

    commited

---
+++ Patter.java
@@ -0,0 +1,11 @@
+
+public class Patter{
+public static void main(String[] args){
+ for(int j=1; j<6; j++){
+  for(int i=6-j; i>1; i--){
+    System.out.print(" ");
+  }
+  System.out.println(j);
+}
+}
+}
